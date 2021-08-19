4f34b6905fe11fd11c39d9978082cb68db6a2f76
ZeeshanZulfiqarAli <zeeshan.dossani23@gmail.com>
2021-08-19 15:12:32 +0500

    Animating the camera on mouse movement

--- src/Components/Root.js
+++ src/Components/Root.js
@@ -12,20 +12,31 @@ const Root = () => {
             height: 600
         }
 
+        // Cursor
+        const cursor = {
+            x: 0,
+            y: 0
+        }
+
+        window.addEventListener('mousemove', (event) =>
+        {
+            cursor.x = event.clientX / sizes.width - 0.5
+            cursor.y = - (event.clientY / sizes.height - 0.5)
+        })
+
         // Scene
         const scene = new THREE.Scene()
 
         // Object
-        const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-        const cubeMaterial = new THREE.MeshBasicMaterial({
-            color: '#ff0000'
-        })
-        const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
-        scene.add(cubeMesh)
+        const mesh = new THREE.Mesh(
+            new THREE.BoxGeometry(1,1,1,5,5,5),
+            new THREE.MeshBasicMaterial({color: 0xff0000})
+        )
+        scene.add(mesh)
 
         // Camera
-        const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-        camera.position.z = 3
+        const camera = new THREE.PerspectiveCamera(55, sizes.width / sizes.height, 0.1, 100)
+
         scene.add(camera)
 
         // Renderer
@@ -33,8 +44,31 @@ const Root = () => {
             canvas: canvas.current
         })
         renderer.setSize(sizes.width, sizes.height)
-        renderer.render(scene, camera)
+        // renderer.render(scene, camera)
 
+        // Animate
+        const clock = new THREE.Clock()
+
+        const tick = () =>
+        {
+            // const elapsedTime = clock.getElapsedTime()
+
+            // Update controls
+            // mesh.rotation.y = elapsedTime
+            // controls.update()
+
+            // Update camera
+            camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+            camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+            camera.position.y = cursor.y * 3
+            camera.lookAt(mesh.position)
+            // Render
+            renderer.render(scene, camera)
+
+            // Call tick again on the next frame
+            window.requestAnimationFrame(tick)
+        }
+        tick()
     }, [])
 
     return (
